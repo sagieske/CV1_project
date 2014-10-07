2a9e42d9279aae4e55cdf78eff51af029d9c9968
Elise <koster.elise@gmail.com>
2014-10-07 19:09:08 +0200

    Can build bag-of-words for each image

--- frame.m
+++ frame.m
@@ -1,19 +1,22 @@
 function frame(train_number)
     amount_per_class = round(train_number/4)
-    airplanes_files = dir('airplanes_train/*.jpg'); 
-    cars_files = dir('cars_train/*.jpg'); 
-    faces_files = dir('faces_train/*.jpg'); 
-    motorbikes_files = dir('motorbikes_train/*.jpg'); 
-    data_matrix = []
+    airplanes_files = dir('data/airplanes_train/*.jpg'); 
+    cars_files = dir('data/cars_train/*.jpg'); 
+    faces_files = dir('data/faces_train/*.jpg'); 
+    motorbikes_files = dir('data/motorbikes_train/*.jpg'); 
+    data_matrix = [];
+    selected_images = [];
     for i = 1:amount_per_class
-        desc1 = extract_features(strcat('airplanes_train/', airplanes_files(i).name), 'key');
-        desc2 = extract_features(strcat('cars_train/', cars_files(i).name), 'key');
-        desc3 = extract_features(strcat('faces_train/', faces_files(i).name), 'key');
-        desc4 = extract_features(strcat('motorbikes_train/', motorbikes_files(i).name), 'key');
+        desc1 = extract_features(strcat('data/airplanes_train/', airplanes_files(i).name), 'key');
+        desc2 = extract_features(strcat('data/cars_train/', cars_files(i).name), 'key');
+        desc3 = extract_features(strcat('data/faces_train/', faces_files(i).name), 'key');
+        desc4 = extract_features(strcat('data/motorbikes_train/', motorbikes_files(i).name), 'key');
         data_matrix = cat(2, data_matrix, desc1, desc2, desc3, desc4);
+        selected_images = cat(1, selected_images, airplanes_files(i).name, cars_files(i).name, faces_files(i).name, motorbikes_files(i).name);
     end
-    build_vocab(im2single(data_matrix), 400);
-    size(data_matrix)
+    [centers, assignment] = build_vocab(im2single(data_matrix), 400);
+    words_matrix = quantize_features(selected_images, centers, assignment);
+    size(words_matrix)
     
         
     
--- quantize_features.m
+++ quantize_features.m
@@ -1,2 +1,13 @@
-function descriptor = quantize_features(vocabulary, training_set)
-    descriptor = 0;
+function words_matrix = quantize_features(images, centers, assignment)
+    amount_images = size(images)
+    words_matrix = [];
+    for i = 1:amount_images
+        descriptor_set = extract_features(images(i,:), 'key');
+        words_vector = []
+        for j = 1:size(descriptor_set,2)
+            [x, k] = min(vl_alldist(single(descriptor_set(:,j)), centers));
+            words_vector = cat(2, words_vector, k);
+        end
+        words_matrix= cat(1, words_matrix, words_vector);
+    end
+    
