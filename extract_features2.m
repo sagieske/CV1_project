0b137ec83c11fde673922f0bb7e40ce901d56333
sagieske <sharongieske@gmail.com>
2014-10-12 22:33:26 +0200

    frame.m now uses extractfeatures2. extractfeatures2 fixed concatenating descriptors for multiple colorspaces

--- extract_features2.m
+++ extract_features2.m
@@ -1,19 +1,26 @@
-% Parameters: im = path to image
-%             type_sift = {dense, key}
-%             color_space = {gray, RGB, normalizedRGB, opponent}
+% Parameters:   im = path to image
+%               type_sift = {dense, key}
+%               color_space = {gray, RGB, normalizedRGB, opponent}
+% Returns:      descriptors =  single if color_space is gray, otherwise list
+%               of 3 descriptors
 
 function descriptors = extract_features2(im, type_sift, color_space)   
     % Color image for different colorspaces
     im_color = imread(im);
     
-    % Calculate descriptors for different types of color spaces using dense
-    % or key based sift
-    
-    % TODO: Maybe add a check in RGB. nRGB and opponent to check whether the
-    % image is really RGB and not accidentially grayscale???
-    % -> Update: check implemented, not sure if it really is required.
-    % Please review.
+
     channels = size(im_color, 3);
+    
+    % Check if image is gray scale eventhough another color space was
+    % needed. For now returns empty set
+    if (channels == 1 && ~strcmp(color_space, 'gray'))
+        descriptors = {}
+        disp('aaaa')
+        return
+    end
+    
+   % Calculate descriptors for different types of color spaces using dense
+    % or key based sift
     switch(color_space)
         case('gray')
             % Check if image is gray or rgb
@@ -29,84 +36,64 @@ function descriptors = extract_features2(im, type_sift, color_space)
                     [~, descriptors] = vl_sift(im_key);
             end
         case('RGB')
-            % Check if input image is in RGB
-            if(channels == 3)
-                % RGBSift (sift per color channel)
-                R = im_color(:,:,1); % Red channel
-                G = im_color(:,:,2); % Green channel
-                B = im_color(:,:,3); % Blue channel
-                switch(type_sift)
-                    case('dense')
-                        [~, R_descriptors] = vl_dsift(im2single(R));
-                        [~, G_descriptors] = vl_dsift(im2single(G));
-                        [~, B_descriptors] = vl_dsift(im2single(B));
-                        descriptors = cat(3, R_descriptors, G_descriptors, B_descriptors);
-                    case('key')
-                        [~, R_descriptors] = vl_sift(im2single(R));
-                        [~, G_descriptors] = vl_sift(im2single(G));
-                        [~, B_descriptors] = vl_sift(im2single(B));
-                        descriptors = cat(3, R_descriptors, G_descriptors, B_descriptors);
-                end
-            else
-                descriptors = [];
+            % RGBSift (sift per color channel)
+            R = im_color(:,:,1); % Red channel
+            G = im_color(:,:,2); % Green channel
+            B = im_color(:,:,3); % Blue channel
+            switch(type_sift)
+                case('dense')
+                    [~, R_descriptors] = vl_dsift(im2single(R));
+                    [~, G_descriptors] = vl_dsift(im2single(G));
+                    [~, B_descriptors] = vl_dsift(im2single(B));
+                case('key')
+                    [~, R_descriptors] = vl_sift(im2single(R));
+                    [~, G_descriptors] = vl_sift(im2single(G));
+                    [~, B_descriptors] = vl_sift(im2single(B));
             end
+            descriptors = {R_descriptors, G_descriptors, B_descriptors};
         case('normalizedRGB')
-            % Check if input image is in RGB
-            if(channels == 3)
-                % rgbSift (sift per normalized color channel)
-                R = im_color(:,:,1); % Red channel
-                G = im_color(:,:,2); % Green channel
-                B = im_color(:,:,3); % Blue channel
-                total = R+G+B;
-                r = double(R./total);
-                g = double(G./total);
-                b = double(B./total);
-                switch(type_sift)
-                    case('dense')
-                        [~, r_descriptors] = vl_dsift(im2single(r));
-                        [~, g_descriptors] = vl_dsift(im2single(g));
-                        [~, b_descriptors] = vl_dsift(im2single(b));
-                        descriptors = cat(3, r_descriptors, g_descriptors, b_descriptors);
-                    case('key')
-                        [~, r_descriptors] = vl_sift(im2single(r));
-                        [~, g_descriptors] = vl_sift(im2single(g));
-                        [~, b_descriptors] = vl_sift(im2single(b));
-                        descriptors = cat(3, r_descriptors, g_descriptors, b_descriptors);
-                end
-            else
-                descriptors = [];
+            % rgbSift (sift per normalized color channel)
+            R = im_color(:,:,1); % Red channel
+            G = im_color(:,:,2); % Green channel
+            B = im_color(:,:,3); % Blue channel
+            total = R+G+B;
+            r = double(R./total);
+            g = double(G./total);
+            b = double(B./total);
+            switch(type_sift)
+                case('dense')
+                    [~, r_descriptors] = vl_dsift(im2single(r));
+                    [~, g_descriptors] = vl_dsift(im2single(g));
+                    [~, b_descriptors] = vl_dsift(im2single(b));
+                case('key')
+                    [~, r_descriptors] = vl_sift(im2single(r));
+                    [~, g_descriptors] = vl_sift(im2single(g));
+                    [~, b_descriptors] = vl_sift(im2single(b));
             end
+            descriptors = {r_descriptors, g_descriptors, b_descriptors};
         case('opponent')
-            % Check if the input image is in RGB
-            if(channels == 3)
-                % opponentSift (sift in opponent color space)
-                R = im_color(:,:,1); % Red channel
-                G = im_color(:,:,2); % Green channel
-                B = im_color(:,:,3); % Blue channel
-                first = (R-G)./(sqrt(2));
-                second = (R+G-(2*B))./(sqrt(6));
-                third = (R+G+B)./sqrt(3);
-                switch(type_sift)
-                    case('dense')
-                        [~, first_descriptors] = vl_dsift(im2single(first));
-                        [~, second_descriptors] = vl_dsift(im2single(second));
-                        [~, third_descriptors] = vl_dsift(im2single(third));
-                        descriptors = cat(3, first_descriptors, second_descriptors, third_descriptors);
-                    case('key')
-                        
+            % opponentSift (sift in opponent color space)
+            R = im_color(:,:,1); % Red channel
+            G = im_color(:,:,2); % Green channel
+            B = im_color(:,:,3); % Blue channel
+            first = (R-G)./(sqrt(2));
+            second = (R+G-(2*B))./(sqrt(6));
+            third = (R+G+B)./sqrt(3);
+            switch(type_sift)
+                case('dense')
+                    [~, first_descriptors] = vl_dsift(im2single(first));
+                    [~, second_descriptors] = vl_dsift(im2single(second));
+                    [~, third_descriptors] = vl_dsift(im2single(third));
+                case('key')
+                    
                         % TODO: Sometimes, one or multiple descriptor
                         % matrices from key sift using opponent color space
                         % are empty, making cat() throw an error. Error
                         % encountered with gray images and images with a
-                        % lot of the same colors.
-                        
-                        
-                        [~, first_descriptors] = vl_sift(im2single(first));
-                        [~, second_descriptors] = vl_sift(im2single(second));
-                        [~, third_descriptors] = vl_sift(im2single(third));
-                        descriptors = cat(3, first_descriptors, second_descriptors, third_descriptors);
-                end
-            else
-                descriptors = [];
+                        % lot of the same colors.                    
+                    [~, first_descriptors] = vl_sift(im2single(first));
+                    [~, second_descriptors] = vl_sift(im2single(second));
+                    [~, third_descriptors] = vl_sift(im2single(third));
             end
+            descriptors = {first_descriptors, second_descriptors, third_descriptors};
     end
--- frame.m
+++ frame.m
@@ -1,4 +1,4 @@
-function frame(train_number)
+function frame(train_number,  type_sift, color_space)
     % Calculate # images for each class dependend on # training images
     amount_per_class = round(train_number/4)
     % Get file information of jpg images from directories
@@ -10,17 +10,24 @@ function frame(train_number)
     selected_images = [];
     % Get desriptors for image from each training class
     for i = 1:amount_per_class
-        desc1 = extract_features(strcat('data/airplanes_train/', airplanes_files(i).name), 'key');
-        desc2 = extract_features(strcat('data/cars_train/', cars_files(i).name), 'key');
-        desc3 = extract_features(strcat('data/faces_train/', faces_files(i).name), 'key');
-        desc4 = extract_features(strcat('data/motorbikes_train/', motorbikes_files(i).name), 'key');
+        desc1 = extract_features2(strcat('data/airplanes_train/', airplanes_files(i).name),  type_sift, color_space);
+        desc2 = extract_features2(strcat('data/cars_train/', cars_files(i).name),  type_sift, color_space);
+        desc3 = extract_features2(strcat('data/faces_train/', faces_files(i).name),  type_sift, color_space);
+        desc4 = extract_features2(strcat('data/motorbikes_train/', motorbikes_files(i).name),  type_sift, color_space);
+
         % Concatenate descriptors
-        data_matrix = cat(2, data_matrix, desc1, desc2, desc3, desc4);
-        % Concatenate names of images used for descriptors
-        selected_images = cat(1, selected_images, airplanes_files(i).name, cars_files(i).name, faces_files(i).name, motorbikes_files(i).name);
+        if (strcmp(color_space,'gray'))
+            data_matrix = cat(2, data_matrix, desc1, desc2, desc3, desc4);
+            % Concatenate names of images used for descriptors
+            selected_images = cat(1, selected_images, airplanes_files(i).name, cars_files(i).name, faces_files(i).name, motorbikes_files(i).name);
+        else
+            break
+        end
+        
     end
     % Build vocabulary with N visual words
     N = 400;
+    size(data_matrix)
     [centers, assignment] = build_vocab(im2single(data_matrix), N);
     % Quantize features 
     words_matrix = quantize_features(selected_images, centers, assignment);
