3ca59868b44f187410a035622736865b4afc44a0
sagieske <sharongieske@gmail.com>
2014-10-12 23:33:05 +0200

    shows 2 different hist out of 8.. strange... but progress?

--- get_histogram.m
+++ get_histogram.m
@@ -2,7 +2,7 @@ function his = get_histogram(words_matrix)
     % For every image, create histogram
     for i = 1:size(words_matrix,2)
         figure,
-        hist(words_matrix{1})
+        hist(words_matrix{i})
         %hist(words_matrix(i,:))
     end
     his=0
