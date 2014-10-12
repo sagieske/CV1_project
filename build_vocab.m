b5b0123e4f07d1bda56781191c49aa7428771742
sagieske <sharongieske@gmail.com>
2014-10-12 22:48:47 +0200

    added comment on size of parameters

--- build_vocab.m
+++ build_vocab.m
@@ -1,2 +1,5 @@
 function [centers, assignment] = build_vocab(data_matrix, clusters)
+    % Center has same number of rows as data_matrix (128) and clustersize number of columns
+    % Assignment is a row vector specifying assignments of data_matrix to
+    % centers
     [centers, assignment] = vl_kmeans(data_matrix, clusters);
