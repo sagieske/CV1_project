db3f431480a5ba6a2c758131427edb9b2b929328
Elise <koster.elise@gmail.com>
2014-10-13 14:32:41 +0200

    Normalization of histograms

--- get_histogram.m
+++ get_histogram.m
@@ -1,8 +1,10 @@
 function his = get_histogram(words_matrix, selected_images)
     % For every image, create histogram
     for i = 1:size(words_matrix,2)
+        size(words_matrix{i})
         figure('name',selected_images{i}),
-        hist(words_matrix{i})
+        %hist(words_matrix{i})
+        bar(hist(words_matrix{i}) ./ sum(hist(words_matrix{i})))
         %hist(words_matrix(i,:))
     end
     his=0
