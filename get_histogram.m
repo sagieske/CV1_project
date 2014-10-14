1be1ad5e50ac4438e94da4b0b0fe401b445201ca
Elise <koster.elise@gmail.com>
2014-10-14 18:21:21 +0200

    Get histogram matrix per class

--- get_histogram.m
+++ get_histogram.m
@@ -1,8 +1,7 @@
 function normalized_matrix = get_histogram(words_matrix)
     % For every image, create histogram
     a = 1:400;
-    size(words_matrix)
-    normalized_matrix = []
+    normalized_matrix = [];
     for i = 1:size(words_matrix,2)
         %figure('name',selected_images{i}),
         %hist(words_matrix{i})
