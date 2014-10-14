9b1226f26d777a183de994980f99104f75e137c5
sagieske <sharongieske@gmail.com>
2014-10-14 17:56:21 +0200

    get datamatrix per class

---
+++ get_descriptors_class.m
@@ -0,0 +1,39 @@
+% input:    N:              number of images from class
+%           classfolder:    path to folder with images of class
+%           type_sift:      
+%           color space:
+
+function datamatrix_class = get_descriptors_class(N, classfolder, imagefile_names,type_sift, color_space)
+    % Get all jpg image information in class folder
+    class_files = dir(strcat(classfolder, '*.jpg')); 
+    selected_images = cell(1,N);
+    
+    % init for channels
+    data_matrix_ch1 = [];
+    data_matrix_ch2 = []; 
+    data_matrix_ch3 = [];
+    
+    data_matrix_class = {}
+    for i = 1:N
+        % Append correct folder
+        imagename = strcat(classfolder, class_files(i).name);
+        selected_image{i} = imagename;
+        % Get descriptors
+        descriptors = extract_features2(imagename,  type_sift, color_space);
+         % There is always at least channel 1
+        data_matrix_ch1 = cat(2, data_matrix_ch1, descriptors{1});
+        %???? Seperate data_matrices for seperate channels?????
+        if (~strcmp(color_space,'gray'))
+            data_matrix_ch2 = cat(2, data_matrix_ch2, descriptors{2});
+            data_matrix_ch3 = cat(2, data_matrix_ch3, descriptors{3});
+        end      
+    end
+    
+    % Put in data_matrix cell array
+    if (strcmp(color_space,'gray'))
+            data_matrix_class{1} = data_matrix_ch1;
+    else
+        data_matrix_class = { data_matrix_ch1, data_matrix_ch2, data_matrix_ch3};
+    end
+    
+end
