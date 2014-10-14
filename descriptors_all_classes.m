94d41ab1501fdffefb44c2a131cadc91e83b5d77
sagieske <sharongieske@gmail.com>
2014-10-14 18:57:55 +0200

    sepereate function to get descriptors for all classes, adjusted in frame

---
+++ descriptors_all_classes.m
@@ -0,0 +1,44 @@
+% Returns   total_data_matrix:      cell array of length # channels, each
+%                                   channel has datamatrices of all classes
+%                                   combined in matrix
+%           selected_images:        cell array of all images that were used (
+%                                   in same order as total_data_matrix
+%           datamatrix_per_class:   cell array of length # classes, each
+%                                   element is a cell array of descriptors (length # channels) per
+%                                   class. Each element i in the descriptor
+%                                   is a data_matrix of all images from
+%                                   that class in channel i
+%           selected_images_p_class cell array of images used for
+%                                   data_matrix_per_class
+
+function [total_data_matrix, selected_images, datamatrix_per_class, selected_images_per_class] =  descriptors_all_classes(amount_per_class, classes, type_sift, color_space)
+
+    if (~strcmp(color_space,'gray'))
+        channels = 3;
+    else
+    	channels = 1;
+    end
+    
+    % Initialize everything
+    number_of_classes = size(classes,2);
+    datamatrix_per_class = cell(1,number_of_classes);
+    selected_images_per_class = cell(1,number_of_classes);
+    total_data_matrix = cell(1,channels);
+
+    % Get number of channels
+    selected_images = {};
+    
+    % Loop over all classes to get data_matrix (of possible multiple
+    % channels per class)
+    for c=1:number_of_classes
+        fprintf('- Retrieving %i images from class %s for training.\n', amount_per_class, classes{c});
+        [matrix, images_names] = get_descriptors_class(amount_per_class, classes{c},type_sift, color_space);
+        datamatrix_per_class{c} = matrix;
+        selected_images_per_class{c} = images_names;
+        % append image file names to cell array
+        selected_images= cat(2 , selected_images , selected_images_per_class{c});
+        % Append to total data matrix for training
+        for i=1:channels
+            total_data_matrix{i} = cat(2, total_data_matrix{i}, matrix{i});
+        end        
+    end
--- frame.m
+++ frame.m
@@ -10,50 +10,15 @@ function frame(train_number,  type_sift, color_space)
     data_matrix_ch2 = [];
     data_matrix_ch3 = [];
 
-    selected_images = cell(1,train_number);
     % Get desriptors for image from each training class
     disp('Extracting features')
-    [data_matrix_airplane, air_im] = get_descriptors_class(amount_per_class, 'data/airplanes_train/', 'key', 'gray');
-    [data_matrix_car, car_im] = get_descriptors_class(amount_per_class, 'data/cars_train/', 'key', 'gray');
-    [data_matrix_face, fac_im] = get_descriptors_class(amount_per_class, 'data/faces_train/', 'key', 'gray');
-    [data_matrix_motorbike, mot_im] = get_descriptors_class(amount_per_class, 'data/motorbikes_train/', 'key', 'gray');
+    classes = {'data/airplanes_train/', 'data/cars_train/', 'data/faces_train/', 'data/motorbikes_train/'} ;
+    [total_data_matrix, selected_images, datamatrix_per_class, selected_images_per_class] =  descriptors_all_classes(amount_per_class, classes, type_sift, color_space)
     
+    % Data matrix for class 1, channel 1: size(datamatrix_class{1}{1})
     
-%     for i = 1:amount_per_class
-%         % Append correct folder
-%         airplanes_name = strcat('data/airplanes_train/', airplanes_files(i).name);
-%         cars_name = strcat('data/cars_train/', cars_files(i).name);
-%         faces_name = strcat('data/faces_train/', faces_files(i).name);
-%         motorbikes_name = strcat('data/motorbikes_train/', motorbikes_files(i).name);
-%         
-%         % Concatenate names of images used for descriptors
-%         selected_images{i*4-3}  = airplanes_name;
-%         selected_images{i*4-2}  =  cars_name;
-%         selected_images{i*4-1}  = faces_name;
-%         selected_images{i*4}  = motorbikes_name;
-% 
-%         % Get descriptors
-%         desc1 = extract_features2(airplanes_name,  type_sift, color_space);
-%         desc2 = extract_features2(cars_name,  type_sift, color_space);
-%         desc3 = extract_features2(faces_name,  type_sift, color_space);
-%         desc4 = extract_features2(motorbikes_name,  type_sift, color_space);
-%         
-%         % There is always at least channel 1
-%         data_matrix_ch1 = cat(2, data_matrix_ch1, desc1{1}, desc2{1}, desc3{1}, desc4{1});
-%         %???? Seperate data_matrices for seperate channels?????
-%         if (~strcmp(color_space,'gray'))
-%             data_matrix_ch2 = cat(2, data_matrix_ch2, desc1{2}, desc2{2}, desc3{2}, desc4{2});
-%             data_matrix_ch3 = cat(2, data_matrix_ch3, desc1{3}, desc2{3}, desc3{3}, desc4{3});
-%         end
-%         
-%     end
-%     
-%     if (strcmp(color_space,'gray'))
-%             data_matrix = {data_matrix_ch1};
-%     else
-%         data_matrix = { data_matrix_ch1, data_matrix_ch2, data_matrix_ch3};
-%     end
-%     
+    data_matrix = total_data_matrix;
+    
     % Build vocabulary with N visual words
     N = 400;
 
@@ -63,8 +28,11 @@ function frame(train_number,  type_sift, color_space)
     % Create seperate word matrices for channels
     data_matrix = [];
     images_matrix = [];
-    data_matrix = cat(2, data_matrix_airplane{1}, data_matrix_car{1}, data_matrix_face{1}, data_matrix_motorbike{1});
-    images_matrix = cat(1, air_im, car_im, fac_im, mot_im);
+    
+    %data_matrix = cat(2, data_matrix_airplane{1}, data_matrix_car{1}, data_matrix_face{1}, data_matrix_motorbike{1});
+    data_matrix = total_data_matrix{1};
+    %images_matrix = cat(1, air_im, car_im, fac_im, mot_im);
+    images_matrix = selected_images;
     
     disp('Building vocab')
     [centers, assignment] = build_vocab(im2single(data_matrix), N);
