edab1442e9513e859daf6e7160ff930105a10c4c
sagieske <sharongieske@gmail.com>
2014-10-26 12:07:55 +0100

    can now test all images(50) from each class

--- testing.m
+++ testing.m
@@ -10,35 +10,40 @@ function prediction = testing(models, centers, assignment, type_sift, color_spac
     % Create dictionary for test models
     class_dictionary_test = create_class_table('test');
     class_names = keys(class_dictionary_test);
-    test_class = 3;
-    class_names(test_class)
+    
+    % TODO total images to be test from each class:
+    total_testimages_per_class = 50;
     
 
-    % Get all descriptors of images
-    [test_desc, test_images] = get_descriptors_class(1,2, class_dictionary_test(char(class_names(3))), type_sift, color_space);
-    % Quantize images using centers and assignment
-    test_svm = quantize_features(test_images, centers, assignment, type_sift, color_space, channel_nr);
+    % Test all svms for every image for every class
+    for k=1:size(class_names,2)
+        class_path = char(class_names(k));
+        % Get all descriptors of images
+        [test_desc, test_images] = get_descriptors_class(1,total_testimages_per_class, class_dictionary_test(class_path), type_sift, color_space);
+        % Quantize images using centers and assignment
+        test_svm = quantize_features(test_images, centers, assignment, type_sift, color_space, channel_nr);
 
-     % Create histograms
-    test_hist = get_histogram(test_svm);
+         % Create histograms
+        test_hist = get_histogram(test_svm);
 
-    size(test_svm,2)
-    disp('testing image:')
-    for j=1:size(test_svm,2)
-         fprintf('\nImage: %s\n', test_images{j});
-        % initialize true value array
-        true_value = zeros(1, size(class_names,2) );
-        true_value(test_class) = 1;
+        disp('testing image:')
+        % Test every image of class on all svms
+        for j=1:size(test_svm,2)
+             fprintf('\nImage: %s\n', test_images{j});
+            % initialize true value array
+            true_value = zeros(1, size(class_names,2) );
+            true_value(k) = 1;
+            prediction = [];
+            % Calculate predictions for each clas and add to prediction array
+            for c=1:size(class_names,2)
+                [pred_test, accuracy, decision_values] = svmpredict(true_value(c), test_hist(j,:), models{c}, ' -b 1');
+                prediction = cat(2,prediction, pred_test);
+            end
 
-        prediction = [];
-        % Calculate predictions for each clas and add to prediction array
-        for c=1:size(class_names,2)
-            [pred_test, accuracy, decision_values] = svmpredict(true_value(c), test_hist(j,:), models{c}, ' -b 1');
-            prediction = cat(2,prediction, pred_test);
-        end
-
-        for i=1:size(prediction,2)
-            fprintf('- Class: %s  = %i (true value: %i)\n', char(class_names(i)), prediction(i), true_value(i));
+            % Print predictions of svm
+            for i=1:size(prediction,2)
+                fprintf('- Class: %s  = %i (true value: %i)\n', char(class_names(i)), prediction(i), true_value(i));
+            end
         end
     end
 end
