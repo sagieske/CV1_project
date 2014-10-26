da869e5840b18597a1d862d12b1710f50837e315
sagieske <sharongieske@gmail.com>
2014-10-26 11:53:36 +0100

    changed dir for project data

--- create_class_table.m
+++ create_class_table.m
@@ -4,13 +4,13 @@ function class_dictionary = create_class_table(usage)
     
     class_dictionary = containers.Map;
     if strcmp(usage, 'training')
-        class_dictionary('airplanes_train') = char('CV1 Project data/data/airplanes_train/');
-        class_dictionary('cars_train') = char('CV1 Project data/data/cars_train/');
-        class_dictionary('faces_train') = char('CV1 Project data/data/faces_train/');
-        class_dictionary('motorbikes_train') = char('CV1 Project data/data/motorbikes_train/');
+        class_dictionary('airplanes_train') = char('CV1 Project/data/airplanes_train/');
+        class_dictionary('cars_train') = char('CV1 Project/data/cars_train/');
+        class_dictionary('faces_train') = char('CV1 Project/data/faces_train/');
+        class_dictionary('motorbikes_train') = char('CV1 Project/data/motorbikes_train/');
     elseif strcmp(usage, 'test')
-        class_dictionary('airplanes_test') = char('CV1 Project data/data/airplanes_test/');
-        class_dictionary('cars_test') = char('CV1 Project data/data/cars_test/');
-        class_dictionary('faces_test') = char('CV1 Project data/data/faces_test/');
-        class_dictionary('motorbikes_test') = char('CV1 Project data/data/motorbikes_test/');
+        class_dictionary('airplanes_test') = char('CV1 Project/data/airplanes_test/');
+        class_dictionary('cars_test') = char('CV1 Project/data/cars_test/');
+        class_dictionary('faces_test') = char('CV1 Project/data/faces_test/');
+        class_dictionary('motorbikes_test') = char('CV1 Project/data/motorbikes_test/');
     end
