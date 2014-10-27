377854fb733c47e4d5c2662e7bd77770c2d0e385
Elise <koster.elise@gmail.com>
2014-10-27 20:19:27 +0100

    Revert to earlier version

--- testing.m
+++ testing.m
@@ -6,44 +6,43 @@
 %               color_space
 %               channel
 % Returns:      Prediction:     array of zero or ones for class prediction 
-function predictions = testing(nr_test_images, models, centers, assignment, type_sift, color_space, channel_nr)
+function prediction = testing(nr_test_images, models, centers, assignment, type_sift, color_space, channel_nr)
     % Create dictionary for test models
     class_dictionary_test = create_class_table('test');
     class_names = keys(class_dictionary_test);
-        
+    
+    % TODO total images to be test from each class:
+    total_testimages_per_class = nr_test_images;
+    
     number_of_classifiers = size(class_names,2);
     
     % Initialize lists of images and their predictions
     predictions_total = cell(1,number_of_classifiers);
     for k=1:number_of_classifiers
         % Initialize cell array for classifier k
-        predictions_total{k} = cell(1,nr_test_images * number_of_classifiers);
+        predictions_total{k} = cell(1,total_testimages_per_class * number_of_classifiers);
     end
     
     counter = 1;
     % Test all svms for every image for every class
-    testimages_total = cell(1,nr_test_images* number_of_classifiers);
+    testimages_total = cell(1,total_testimages_per_class* number_of_classifiers);
     % Initialize: Each predition and true value array of a classifier is stored in predictions
     predictions = cell(1,number_of_classifiers);
     d_values = cell(1, number_of_classifiers);
     true_value = cell(1,number_of_classifiers);
 
-%     for i=1:number_of_classifiers
-%         predictions{i} = [];
-%         d_values{i} = [];
-%         true_values{i} = [];
-%     end
+    for i=1:number_of_classifiers
+        predictions{i} = [];
+        d_values{i} = [];
+        true_values{i} = [];
+    end
     
-    % array to get all histograms
-    total_hist = [];
     
-    total_images  = [];
-    % Get all histograms for images for each classifier. Append to total_hist
     for k=1:number_of_classifiers
-        % Get class to retrieve descriptors
+        
         class_path = char(class_names(k));
         % Get all descriptors of images
-        [test_desc, test_images] = get_descriptors_class(1,nr_test_images, class_dictionary_test(class_path), type_sift, color_space);
+        [test_desc, test_images] = get_descriptors_class(1,total_testimages_per_class, class_dictionary_test(class_path), type_sift, color_space);
         % Quantize images using centers and assignment
         
         if(~strcmp(color_space, 'gray'))
@@ -65,31 +64,42 @@ function predictions = testing(nr_test_images, models, centers, assignment, type
 
              % Create histograms
             test_hist = get_histogram(test_svm);
-            % Append histograms and testimages to total list
-            total_hist = cat(1, total_hist, test_hist);
-            total_images = cat(1,total_images , transpose(test_images));
         end
-    end
-    
-    % Convert array to cell array
-    testimages_total = cellstr(total_images);
-    
-    % Generate true values array
-    true_values = cell(1, number_of_classifiers);
-    correct = ones(nr_test_images,1);
-    false = zeros(nr_test_images,1);
-    
-    true_values{1} = cat(1, correct, false, false, false);
-    true_values{2} = cat(1, false, correct, false, false);
-    true_values{3} = cat(1, false, false, correct, false);
-    true_values{4} = cat(1, false, false, false, correct);
         
-    % Test all for each classifier
-    for classifier=1:number_of_classifiers
-        [pred_test, accuracy, decision_values] = svmpredict(true_values{classifier}, total_hist, models{classifier}, ' -b 1 -q'); 
-        predictions{classifier} = pred_test;
+        
+
+        %disp('testing image:')
+        
+        % Test every image of class on all svms
+        for j=1:size(test_svm,2)
+            % Add testimage to list
+            testimages_total{counter} = test_images{j};
+            %fprintf('\nImage: %s\n', test_images{j});
+            % initialize true value array
+            true_value = zeros(1, size(class_names,2) );
+            true_value(k) = 1;
+            prediction = [];
+            % Calculate predictions for each clas and add to prediction array
+            for c=1:number_of_classifiers
+                [pred_test, accuracy, decision_values] = svmpredict(true_value(c), test_hist(j,:), models{c}, ' -b 1 -q');                
+                % Add predicted value to list
+                old_array = predictions{c};
+                old_d_value = d_values{c};
+                predictions{c} = cat(2,old_array, pred_test);
+                d_values{c} = cat(2, old_d_value, decision_values(2));
+                % Add true values to list
+                old_array_true = true_values{c};
+                true_values{c} = cat(2,old_array_true, true_value(c));
+            end
+            
+            %Increase counter for index of image in predictions_total
+            counter = counter +1;
+
+        end
+
     end
-    
+
+    %d_values{1}
     
     total_sorted_true_vals = cell(1, number_of_classifiers);
     total_sorted_predictions = cell(1, number_of_classifiers);
@@ -101,7 +111,7 @@ function predictions = testing(nr_test_images, models, centers, assignment, type
         all_mat{1} = predictions{c};
         all_mat{2} = true_values{c};
         all_mat{3} = d_values{c};
-        all_mat{4} = testimages_total
+        all_mat{4} = testimages_total;
         [sort_all, indices] = sort(all_mat{3}, 'descend');
         if(c==1)
             all_mat_sort = {fliplr(all_mat{1}(indices)), fliplr(all_mat{2}(indices)), sort_all, fliplr(all_mat{4}(indices))}
@@ -148,5 +158,3 @@ function predictions = testing(nr_test_images, models, centers, assignment, type
     av_mean
     av_mean_p = sum(av_mean)/4
 end
-
-
